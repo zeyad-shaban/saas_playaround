16a5487de00e0a72bfb5ffc35a510063f60fc82e
Zeyad Y. Shaban <64459068+zeyad-shaban@users.noreply.github.com>
2026-05-06 19:14:51 +0300

    Auth branch 1 (#2) Added authentication
    * added LOL!!!
    
    * made url variable according to enviornment
    
    * removed teh useless console log
    
    * fixed the bad url within next.config.ts
    
    * event stream instead of plain text
    
    * test2
    
    * fixed routing broken for streaming API on vercel end
    
    * frontend side authentication added
    
    * authentication added server side
    
    * gitignore updated to ignore pythonc ache files

--- api/index.py
+++ api/index.py
@@ -1,19 +1,25 @@
+from typing import Any
+from dotenv import load_dotenv
+_ = load_dotenv(dotenv_path=".env.local")
+
 import uvicorn
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from fastapi.responses import StreamingResponse
 from google.genai import Client
 from google.api_core import exceptions as google_exceptions
 import os
-from dotenv import load_dotenv
+from .auth import verify_token
 
-_ = load_dotenv(dotenv_path=".env.local")
 
 app = FastAPI()
 client = Client(api_key=os.environ.get("GEMINI_API_KEY"))
 
 
 @app.get("/api/python/idea", response_class=StreamingResponse)
-async def idea():
+async def idea(user_data: dict[str, Any] = Depends(verify_token)):
+    user_id = user_data.get("sub")
+    print(f"Generating idea for user: {user_id}")
+    
     async def stream_generator():
         try:
             prompt = "Give me a great unexpected idea to become mega rich!"
